28b8249e793250ca6c79c5050edc904cc7bf12ba
nagacharankumarreddy <nagacharankumarreddy@gmail.com>
2026-07-19 23:12:43 +0530

    Update Vite base for GitHub User Site

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/my-portfolio/",
   plugins: [react()],
   build: {
     outDir: "dist",
